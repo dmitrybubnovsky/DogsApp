26f0e7666230665fb9b232f22ac4b6e20a887413
StanlyT <55555.tango@gmail.com>
2018-07-30 11:56:07 +0300

    FAT-1782 Удалено MainActivity

--- app/src/main/java/com/andersen/dogsapp/dogs/ui/MenuActivity.java
+++ app/src/main/java/com/andersen/dogsapp/dogs/ui/MenuActivity.java
@@ -1,10 +1,12 @@
 package com.andersen.dogsapp.dogs.ui;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 
 import com.andersen.dogsapp.R;
 
+@SuppressLint("Registered")
 public class MenuActivity extends AppCompatActivity {
 
     @Override
