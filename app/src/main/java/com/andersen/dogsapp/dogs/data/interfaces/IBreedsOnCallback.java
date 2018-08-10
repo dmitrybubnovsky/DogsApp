ee758826c14c4a24bdf3544f46f9e21d0233ad70
StanlyT <55555.tango@gmail.com>
2018-08-10 15:39:23 +0300

    FAT-1858 Удален лишний интерфейс

--- app/src/main/java/com/andersen/dogsapp/dogs/data/interfaces/IBreedsOnCallback.java
+++
@@ -1,9 +0,0 @@
-package com.andersen.dogsapp.dogs.data.interfaces;
-
-import com.andersen.dogsapp.dogs.data.entities.DogKind;
-
-import java.util.List;
-
-public interface IBreedsOnCallback {
-    void breedsCallBack(List<DogKind> dogKinds);
-}
